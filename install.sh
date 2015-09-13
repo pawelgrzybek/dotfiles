06d3fce027c29abeef79c5f35c3adad219c87aef
Pawel Grzybek <studiorgb@users.noreply.github.com>
2015-09-13 19:51:57 +0100

    ESlint and scss-lint configuration added

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Variables
 dir=~/.dotfiles          # dotfiles directory
 olddir=~/.dotfiles_old   # old dotfiles backup directory
-files=".bash_profile .hushlogin"    # list of files/folders to symlink in homedir
+files=".bash_profile .hushlogin .eslintrc .scss-lint.yml"    # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
