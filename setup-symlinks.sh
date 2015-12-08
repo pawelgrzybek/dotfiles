849df127c5e161ab2be2414fbd865ddd7e4ad29c
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2015-12-08 02:50:29 +0000

    add .vimrc file for install process

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -4,36 +4,17 @@
 # Variables
 dir=~/.dotfiles
 dirold=~/.dotfiles_backup
-files=".bash_profile .hushlogin .eslintrc .scss-lint.yml"
-
-# create dotfiles_backup in homedir
-echo "Creating $dirold for backup of any existing dotfiles in ~"
-mkdir -p $dirold
+files=".bash_profile .hushlogin .eslintrc .scss-lint.yml .gitconfig .vimrc"
 
 # Lets make it look nicer
-echo " "
 echo "* * * * * * * * * * * *"
-echo " "
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 
 # Lets make it look nicer
-echo " "
 echo "* * * * * * * * * * * *"
-echo " "
-
-# move any existing dotfiles in homedir to dotfiles_backup directory
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $dirold"
-    mv ~/$file $dirold
-done
-
-# Lets make it look nicer
-echo " "
-echo "* * * * * * * * * * * *"
-echo " "
 
 # Create symlinks
 for file in $files; do
@@ -42,9 +23,7 @@ for file in $files; do
 done
 
 # Lets make it look nicer
-echo " "
 echo "* * * * * * * * * * * *"
-echo " "
 echo "All done :-)"
 echo " "
 
