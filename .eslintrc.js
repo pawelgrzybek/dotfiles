41250af8a33eea4c45eac16110f23a50ab8f097b
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2016-02-17 11:54:59 +0000

    .eslintrc.js as a new ESLint config file, replacement for YAML .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,128 +1,305 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true,
-    "jquery": true
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+    'jquery': true
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true,
-      "impliedStrict": true
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true,
+      'impliedStrict': true
     },
-    "sourceType": "module"
+    'sourceType': 'module'
   },
-  "plugins": [
-    "react"
+  'plugins': [
+    'react'
   ],
-  "rules": {
-    "linebreak-style": [
+  'rules': {
+    'comma-dangle': [
       2,
-      "unix"
+      'never'
     ],
-    "array-bracket-spacing": [
-      1,
-      "never"
+    'no-console': [
+      1
     ],
-    "block-spacing": [
-      1,
-      "always"
+    'no-debugger': [
+      1
     ],
-    "brace-style": [
-      1,
-      "stroustrup",
-      {
-        "allowSingleLine": false
-      }
-    ],
-    "camelcase": [
+    'no-dupe-args': [
       2
     ],
-    "comma-spacing": [
-      2,
-      {
-        "before": false,
-        "after": true
-      }
+    'no-dupe-keys': [
+      2
     ],
-    "comma-style": [
-      2,
-      "last"
+    'no-duplicate-case': [
+      2
     ],
-    "computed-property-spacing": [
-      2,
-      "never"
+    'no-empty': [
+      2
     ],
-    "indent": [
+    'no-extra-boolean-cast': [
+      2
+    ],
+    'no-extra-parens': [
+      2,
+      'all'
+    ],
+    'no-extra-semi': [
+      2
+    ],
+    'no-func-assign': [
+      2
+    ],
+    'no-inner-declarations': [
       1,
+      'both'
+    ],
+    'no-sparse-arrays': [
       2
     ],
-    "newline-after-var": [
-      0,
-      "always"
-    ],
-    "no-console": [
-      0
-    ],
-    "no-mixed-spaces-and-tabs": [
-      2,
-      "smart-tabs"
-    ],
-    "no-nested-ternary": [
+    'no-unexpected-multiline': [
       2
     ],
-    "object-curly-spacing": [
-      2,
-      "always"
-    ],
-    "one-var": [
-      2,
-      "never"
-    ],
-    "quotes": [
-      2,
-      "single"
-    ],
-    "semi-spacing": [
-      2,
-      {
-        "before": false,
-        "after": true
-      }
-    ],
-    "semi": [
-      2,
-      "always"
-    ],
-    "keyword-spacing": [
+    'no-unreachable': [
       2
     ],
-    "space-before-blocks": [
+    'use-isnan': [
       2
     ],
-    "space-before-function-paren": [
-      2,
-      "never"
+    'valid-typeof': [
+      2
     ],
-    "space-in-parens": [
-      2,
-      "never"
+    'consistent-return': [
+      2
     ],
-    "space-infix-ops": [
-      2,
-      {
-        "int32Hint": false
-      }
+    'curly': [
+      2
     ],
-    "space-unary-ops": [
+    'default-case': [
+      2
+    ],
+    'eqeqeq': [
+      2,
+      'smart'
+    ],
+    'no-caller': [
+      2
+    ],
+    'no-empty-function': [
+      2
+    ],
+    'no-empty-pattern': [
+      2
+    ],
+    'no-eq-null': [
+      2
+    ],
+    'no-eval': [
+      2
+    ],
+    'no-extra-label': [
+      2
+    ],
+    'no-floating-decimal': [
+      2
+    ],
+    'no-implied-eval': [
+      2
+    ],
+    'no-loop-func': [
+      2
+    ],
+    'no-multi-spaces': [
+      2
+    ],
+    'no-proto': [
+      2
+    ],
+    'no-redeclare': [
+      2
+    ],
+    'no-self-assign': [
+      2
+    ],
+    'no-self-compare': [
+      2
+    ],
+    'no-sequences': [
+      2
+    ],
+    'no-with': [
+      2
+    ],
+    'wrap-iife': [
+      2,
+      'outside'
+    ],
+    'init-declarations': [
+      2,
+      'always'
+    ],
+    'no-undef': [
+      2
+    ],
+    'no-undef-init': [
+      2
+    ],
+    'no-unused-vars': [
+      2
+    ],
+    'no-use-before-define': [
+      2
+    ],
+    'array-bracket-spacing': [
       1,
+      'never'
+    ],
+    'block-spacing': [
+      2,
+      'always'
+    ],
+    'brace-style': [
+      2,
+      'stroustrup',
       {
-        "words": true,
-        "nonwords": false
+        'allowSingleLine': false
       }
     ],
+    'camelcase': [
+      2
+    ],
+    'comma-spacing': [
+      2,
+      {
+        'before': false,
+        'after': true
+      }
+    ],
+    'comma-style': [
+      2,
+      'last'
+    ],
+    'computed-property-spacing': [
+      2,
+      'never'
+    ],
+    'eol-last': [
+      2
+    ],
+    'indent': [
+      2,
+      2
+    ],
+    'jsx-quotes': [
+      2,
+      'prefer-single'
+    ],
+    'key-spacing': [
+      2,
+      {
+        'beforeColon': false,
+        'afterColon': true
+      }
+    ],
+    'keyword-spacing': [
+      2,
+      {
+        'before': false,
+        'after': false
+      }
+    ],
+    'linebreak-style': [
+      2,
+      'unix'
+    ],
+    'new-cap': [
+      2
+    ],
+    'new-parens': [
+      2
+    ],
+    'no-mixed-spaces-and-tabs': [
+      2
+    ],
+    'no-nested-ternary': [
+      2
+    ],
+    'no-spaced-func': [
+      2
+    ],
+    'no-trailing-spaces': [
+      2
+    ],
+    'no-whitespace-before-property': [
+      2
+    ],
+    'object-curly-spacing': [
+      2,
+      'always'
+    ],
+    'one-var': [
+      2,
+      'never'
+    ],
+    'one-var-declaration-per-line': [
+      2,
+      'always'
+    ],
+    'operator-assignment': [
+      2,
+      'always'
+    ],
+    'padded-blocks': [
+      2,
+      'never'
+    ],
+    'quotes': [
+      2,
+      'single'
+    ],
+    'semi': [
+      2,
+      'always'
+    ],
+    'semi-spacing': [
+      2,
+      {
+        'before': false,
+        'after': false
+      }
+    ],
+    'space-before-blocks': [
+      2
+    ],
+    'space-before-function-paren': [
+      2,
+      'never'
+    ],
+    'space-in-parens': [
+      2,
+      'never'
+    ],
+    'space-infix-ops': [
+      2
+    ],
+    'space-unary-ops': [
+      2,
+      {
+        'words': true,
+        'nonwords': false
+      }
+    ],
+    'spaced-comment': [
+      2,
+      'always'
+    ],
+    'template-curly-spacing': [
+      2,
+      'never'
+    ]
   }
 };
