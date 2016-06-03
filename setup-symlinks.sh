97c43442bba9cfb05bb995010594356bce07e597
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2016-06-03 12:47:06 +0100

    change some spacing only

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -22,8 +22,8 @@ echo ""
 
 # Create symlinks
 for file in $files; do
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/$file
 done
 
 # Lets make it look nicer
