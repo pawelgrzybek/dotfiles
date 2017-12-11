927875d6fd82966e33db81bd9ef6ea339f1b5fd9
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2017-12-11 16:47:06 +0000

    clean up

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,95 +1,78 @@
-# System Preferences > General > Click in the scrollbar to:
-# Jump to the next page
-# defaults write -globalDomain "AppleScrollerPagingBehavior" -bool false
-# Jump to the spot that's clicked
+# System Preferences > General > Click in the scrollbar to: Jump to the spot that's clicked
 defaults write -globalDomain "AppleScrollerPagingBehavior" -bool true
 
-# Finder: show all filename extensions
-# http://www.defaults-write.com/display-the-file-extensions-in-finder/
-# Revert to the default
-# defaults write NSGlobalDomain AppleShowAllExtensions -bool false
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# System Preferences > Desktop & Screen Saver > Start after: Never
+defaults -currentHost write com.apple.screensaver idleTime -int 0
 
-# Finder: show path bar
-defaults write com.apple.finder ShowPathbar -bool true
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-
-# Disable the extension change warning in OS X Finder
-# http://www.defaults-write.com/disable-the-extension-change-warning-in-os-x-finder/
-# Revert to the default
-# defaults write com.apple.finder FXEnableExtensionChangeWarning -bool true
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-
-# Enable snap-to-grid for icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-
-
-# Use list view in all Finder windows by default
-# http://www.defaults-write.com/change-default-view-style-in-os-x-finder/
-# Revert to the default
-# defaults write com.apple.Finder FXPreferredViewStyle icnv
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-
-# Disable the warning before emptying the Trash
-# http://www.defaults-write.com/disable-the-warning-before-emptying-the-trash/
-# Revert to the default
-# defaults write com.apple.finder WarnOnEmptyTrash -bool true
-defaults write com.apple.finder WarnOnEmptyTrash -bool false
-
-
-# Set the icon size of Dock items to 44 pixels
+# System Preferences > Dock > Size:
 defaults write com.apple.dock tilesize -int 36
 
+# System Preferences > Dock > Magnification:
+defaults write com.apple.dock magnification -bool true
 
-# Minimize windows into their application’s icon
+# System Preferences > Dock > Size (magnified):
+defaults write com.apple.dock largesize -int 72
+
+# System Preferences > Dock > Minimize windows using: Scale effect
+defaults write com.apple.dock mineffect -string "scale"
+
+# System Preferences > Dock > Minimize windows into application icon
 defaults write com.apple.dock minimize-to-application -bool true
 
-
-# Show indicator lights for open applications in the Dock
-defaults write com.apple.dock show-process-indicators -bool true
-
-
-# Speed up Mission Control animations
-defaults write com.apple.dock expose-animation-duration -float 0.1
-
-
-# Disable Dashboard
-defaults write com.apple.dashboard mcx-disabled -bool true
-
-
-# Disable Dashboard as a space in Mission Control
-# http://www.defaults-write.com/disable-dashboard-as-a-space-in-mission-control/
-# Revert to the default
-# defaults write com.apple.dock dashboard-in-overlay -bool false
-defaults write com.apple.dock dashboard-in-overlay -bool true
-
-
-# Don’t automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
-
-
-# Remove the Dock Auto-Hide & Show Delay
-# http://www.defaults-write.com/remove-the-dock-auto-hide-show-delay/
-# Revert to the default
-# defaults delete com.apple.Dock autohide-delay
-defaults write com.apple.dock autohide-delay -float 0
-
-# Reduce the animation when hiding/showing the Dock to 0.5s
-defaults write com.apple.dock autohide-time-modifier -float 0.5
-
-
-# Automatically hide and show the Dock
+# System Preferences > Dock > Automatically hide and show the Dock:
 defaults write com.apple.dock autohide -bool true
 
+# System Preferences > Dock > Automatically hide and show the Dock (duration)
+defaults write com.apple.dock autohide-time-modifier -float 0.5
 
+# System Preferences > Dock > Automatically hide and show the Dock (delay)
+defaults write com.apple.dock autohide-delay -float 0
+
+# System Preferences > Dock > Show indicators for open applications
+defaults write com.apple.dock show-process-indicators -bool true
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# System Preferences > Mission Controll > Automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# System Preferences > Mission Controll > Dashboard
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Finder > Preferences > Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder > Preferences > Show wraning before changing an extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Finder > Preferences > Show wraning before removing from iCloud Drive
+defaults write com.apple.finder FXEnableRemoveFromICloudDriveWarning -bool false
+
+# Finder > View > As List
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Finder > View > Show Path Bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Others:
+
+# Completely Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Kill affected apps
 for app in "Dock" "Finder"; do
   killall "${app}" > /dev/null 2>&1
 done
 
-
+# Done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
