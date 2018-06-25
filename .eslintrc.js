4054f82ec2e25e8995fe51e97c8036d385b7151a
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-06-25 07:57:09 +0100

    New ESLint - Possible Errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,12 +39,45 @@ module.exports = {
     'react'
   ],
 
-  extends: 'eslint:recommended',
+  // extends: 'eslint:recommended',
 
   rules: {
     // Possible Errors
-    'no-console': 1,
+    // https://eslint.org/docs/rules/#possible-errors
+    'for-direction': 2,
+    'getter-return': 2,
+    'no-await-in-loop': 2,
+    'no-compare-neg-zero': 2,
+    'no-cond-assign': 2,
+    'no-console': 2,
+    'no-constant-condition': 2,
+    'no-control-regex': 2,
+    'no-debugger': 2,
+    'no-dupe-args': 2,
+    'no-dupe-keys': 2,
+    'no-duplicate-case': 2,
+    'no-empty': 2,
+    'no-empty-character-class': 2,
+    'no-ex-assign': 2,
+    'no-extra-boolean-cast': 2,
+    // 'no-extra-parens': 0,
+    'no-extra-semi': 2,
+    'no-func-assign': 2,
+    'no-inner-declarations': 2,
+    'no-invalid-regexp': 2,
+    'no-irregular-whitespace': 2,
+    'no-obj-calls': 2,
+    'no-prototype-builtins': 2,
+    'no-regex-spaces': 2,
+    'no-sparse-arrays': 2,
     'no-template-curly-in-string': 2,
+    'no-unexpected-multiline': 2,
+    'no-unreachable': 2,
+    'no-unsafe-finally': 2,
+    'no-unsafe-negation': 2,
+    'use-isnan': 2,
+    'valid-jsdoc': 1,
+    'valid-typeof': 2,
 
     // Best Practices
     'array-callback-return': 2,
