28b00ef7a17806e8e4d2c7c09c9335b1229cf1c0
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2019-09-27 06:09:12 +0100

    kiss

--- .hyper.js
+++ .hyper.js
@@ -4,72 +4,17 @@
 
 module.exports = {
   config: {
-    // choose either `'stable'` for receiving highly polished,
-    // or `'canary'` for less polished but more frequent updates
-    updateChannel: "stable",
-
-    // default font size in pixels for all tabs
     fontSize: 16,
-
-    // font family with optional fallbacks
     fontFamily:
       'Dank Mono, Operator Mono, Menlo, Monaco, "Courier New", monospace',
-
-    // default font weight: 'normal' or 'bold'
-    fontWeight: "normal",
-
-    // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: "bold",
-
-    // line height as a relative unit
     lineHeight: 2,
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: "#D4D4D4",
-
-    // terminal text color under BLOCK cursor
-    cursorAccentColor: "#000",
-
-    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
     cursorShape: "BEAM",
-
-    // set to `true` (without backticks and without quotes) for blinking cursor
-    cursorBlink: false,
-
-    // color of the text
     foregroundColor: "#D4D4D4",
-
-    // terminal background color
-    // opacity is only supported on macOS
     backgroundColor: "#242B2E",
-
-    // terminal selection color
     selectionColor: "#FFFFFF3D",
-
-    // border color (window, tabs)
     borderColor: "#30373A",
-
-    // custom CSS to embed in the main window
-    css: "",
-
-    // custom CSS to embed in the terminal window
-    termCSS: "",
-
-    // if you're using a Linux setup which show native menus, set to false
-    // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: "",
-
-    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: "",
-
-    // custom padding (CSS format, i.e.: `top right bottom left`)
     padding: "16px 16px",
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
     colors: {
       black: "#7F7F7F",
       red: "#E15A60",
@@ -88,64 +33,9 @@ module.exports = {
       lightCyan: "#5FB3B3",
       lightWhite: "#D4D4D4"
     },
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // PowerShell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: "zsh",
-
-    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
-    // by default `['--login']` will be used
-    shellArgs: ["--login"],
-
-    scrollback: 5000,
-
-    // for environment variables
-    env: {},
-
-    // set to `false` for no bell
-    bell: "SOUND",
-
-    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
-
-    // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-    defaultSSHApp: true
-
-    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
-    // selection is present (`true` by default on Windows and disables the context menu feature)
-    // quickEdit: true,
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
+    scrollback: 5000
   },
-
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
   plugins: [],
-
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
   localPlugins: [],
-
-  keymaps: {
-    // Example
-    // 'window:devtools': 'cmd+alt+o',
-  }
+  keymaps: {}
 };
