d0e37d9d70e2d90d18289b75f95978ce70910900
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2021-09-30 17:46:46 +0100

    replace alacritty with hyper

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -7,8 +7,7 @@ ln -s ~/.dotfiles/Capture\ One/* ~/Library/Application\ Support/Capture\ One
 # Dotfiles
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.hyper.js ~/.hyper.js
 
 # Initialize new settings
 source ~/.zshrc
