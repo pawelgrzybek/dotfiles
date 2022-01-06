e9b0b0d2186decf9ca41223bad3d658e9c445164
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2022-01-06 12:14:06 +0000

    Update brew setup script

--- setup-brew.sh
+++ setup-brew.sh
@@ -1,23 +1,20 @@
 # Upgrade brew
 brew upgrade
 
-# Add some casks
-brew tap joedrago/repo
-
 # Install CLI tools
-brew install joedrago/repo/avifenc
 brew install awscli
 brew install deno
+brew install docker
 brew install git
 brew install hugo
 brew install nvm
+brew install rustup-init
 brew install webp
 brew install yarn
 
-# Install CLI tools
+# Install casks
 brew install 1password
 brew install cleanmymac
-brew install docker
 brew install figma
 brew install forklift
 brew install google-chrome
@@ -26,11 +23,17 @@ brew install imageoptim
 brew install insomnia
 brew install kap
 brew install keybase
-brew install signal
-brew install slack
 brew install openvpn-connect
 brew install raycast
+brew install signal
+brew install slack
 brew install visual-studio-code
+
+# Add some taps
+brew tap joedrago/repo
+
+# Install tap formulas
+brew install joedrago/repo/avifenc
 
 # Remove outdated versions from the cellar.
 brew cleanup
