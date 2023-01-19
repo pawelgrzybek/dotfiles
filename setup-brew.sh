be89a61b9410622287501bb2ec2e9fe456d3e6f4
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2023-01-19 11:17:01 +0000

    Update brew script

--- setup-brew.sh
+++ setup-brew.sh
@@ -10,6 +10,8 @@ brew install awscli
 brew install deno
 brew install git
 brew install hugo
+brew install jq
+brew install mongosh
 brew install nvm
 brew install rustup-init
 brew install webp
@@ -22,13 +24,16 @@ brew install --cask docker
 brew install figma
 brew install forklift
 brew install google-chrome
+brew install google-chrome-canary
+brew install --cask httpie
 brew install imageoptim
-brew install insomnia
 brew install iterm2
 brew install mongodb-compass
+brew install openvpn-connect
 brew install raycast
 brew install safari-technology-preview
 brew install screenflow
+brew install shapr3d
 brew install signal
 brew install visual-studio-code
 
