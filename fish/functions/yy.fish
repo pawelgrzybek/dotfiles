387bb0d81522d6e5d8feb5b34ca27bc8405d0d4b
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-08-16 16:18:58 +0100

    add yy functions

---
+++ fish/functions/yy.fish
@@ -0,0 +1,8 @@
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
