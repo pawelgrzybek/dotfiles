2c7acd88ad0534973cbbb64da7a8b15458ad3018
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-08-17 05:59:35 +0100

    add yazi config

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -6,6 +6,7 @@ ln -s ~/.dotfiles/fish ~/.config/
 ln -s ~/.dotfiles/helix ~/.config/
 ln -s ~/.dotfiles/zellij ~/.config/
 ln -s ~/.dotfiles/lazygit ~/.config/
+ln -s ~/.dotfiles/yazi ~/.config/
 
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
