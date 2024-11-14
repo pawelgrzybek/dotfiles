1832eed710dc4ea8b10fbba6bab6d2cbcdaccd83
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-11-14 20:39:43 +0000

    change theme

--- nvim/lua/pawelgrzybek/plugins/rose-pine.lua
+++ nvim/lua/pawelgrzybek/plugins/rose-pine.lua
@@ -3,13 +3,13 @@ return { -- You can easily change to a different colorscheme.
 	-- change the command in the config to whatever the name of that colorscheme is.
 	--
 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	"kyazdani42/blue-moon",
+	"rose-pine/neovim",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 	init = function()
 		-- Load the colorscheme here.
 		-- Like many other themes, this one has different styles, and you could load
 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		vim.cmd.colorscheme("blue-moon")
+		vim.cmd.colorscheme("rose-pine")
 
 		-- You can configure highlights by doing something like:
 		-- vim.cmd.hi("Comment gui=none")
