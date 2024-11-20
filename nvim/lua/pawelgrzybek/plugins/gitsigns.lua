6786c82254b5c519a2f3bcdd8fd0c69227c3ec05
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-11-20 19:18:59 +0000

    add cool shortcuts to gitsigns

--- nvim/lua/pawelgrzybek/plugins/gitsigns.lua
+++ nvim/lua/pawelgrzybek/plugins/gitsigns.lua
@@ -5,4 +5,27 @@ return {
 			delay = 300,
 		},
 	},
+	config = function()
+		require("gitsigns").setup({
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				vim.keymap.set("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end, { buffer = bufnr })
+
+				vim.keymap.set("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end, { buffer = bufnr })
+			end,
+		})
+	end,
 }
