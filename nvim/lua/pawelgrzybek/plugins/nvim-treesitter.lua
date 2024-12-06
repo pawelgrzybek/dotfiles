0c50c6e1f63a7a42d02f95e404bf1dd3ddb7d497
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-12-06 11:22:50 +0000

    treesitter up

--- nvim/lua/pawelgrzybek/plugins/nvim-treesitter.lua
+++ nvim/lua/pawelgrzybek/plugins/nvim-treesitter.lua
@@ -1,35 +1,32 @@
 return { -- Highlight, edit, and navigate code
+
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	main = "nvim-treesitter.configs", -- Sets main module to use for opts
-	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 	opts = {
 		ensure_installed = {
-			"javascript",
-			"typescript",
-			"tsx",
-			"json",
 			"astro",
-			"html",
-			"css",
-			"scss",
 			"bash",
+			"css",
 			"fish",
+			"go",
+			"html",
+			"javascript",
+			"json",
 			"lua",
 			"luadoc",
-			"rust",
-			"go",
 			"markdown",
 			"markdown_inline",
+			"rust",
+			"scss",
+			"tsx",
+			"typescript",
 			"vim",
 			"vimdoc",
 		},
 		-- Autoinstall languages that are not installed
 		highlight = {
 			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
 			additional_vim_regex_highlighting = { "ruby" },
 		},
 		indent = { enable = true, disable = { "ruby" } },
@@ -38,15 +35,9 @@ return { -- Highlight, edit, and navigate code
 			keymaps = {
 				init_selection = "<A-o>",
 				node_incremental = "<A-o>",
-				scope_incremental = false,
+				scope_incremental = "<A-O>",
 				node_decremental = "<A-i>",
 			},
 		},
 	},
-	-- There are additional nvim-treesitter modules that you can use to interact
-	-- with nvim-treesitter. You should go explore a few and see what interests you:
-	--
-	--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-	--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-	--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
