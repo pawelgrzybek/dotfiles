1ad99eb0be22ac3c8d91200a58e56c0a1a5164e7
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-12-06 10:18:45 +0000

    statusline simler

--- nvim/lua/pawelgrzybek/plugins/mini.lua
+++ nvim/lua/pawelgrzybek/plugins/mini.lua
@@ -19,6 +19,7 @@ return { -- Collection of various small independent plugins/modules
 		-- Simple and easy statusline.
 		--  You could remove this setup call if you don't like it,
 		--  and try some other statusline plugin
+		require("mini.statusline").setup()
 
 		-- set use_icons to true if you have a Nerd Font
 		-- statusline.setup({ use_icons = vim.g.have_nerd_font })
