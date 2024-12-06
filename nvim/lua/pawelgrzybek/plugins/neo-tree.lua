e50a69bf62c1ef4f389c84c63b6a3a31dfb4b889
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-12-06 07:37:25 +0000

    up

--- nvim/lua/pawelgrzybek/plugins/neo-tree.lua
+++ nvim/lua/pawelgrzybek/plugins/neo-tree.lua
@@ -7,17 +7,27 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	cmd = "Neotree",
+	opts = {
+		window = {
+			width = 50,
+		},
+	},
 	keys = {
 		{ "\\", ":Neotree toggle last<CR>", desc = "[E]xplorer toggle", silent = true },
-		{ "|", function()
-        if vim.bo.filetype == "neo-tree" then
-            vim.cmd.wincmd("p")
-          else
-            vim.cmd("Neotree focus last")
-        end
-    end, desc = "[E]xplorer focus", silent = true },
+		{
+			"|",
+			function()
+				if vim.bo.filetype == "neo-tree" then
+					vim.cmd.wincmd("p")
+				else
+					vim.cmd("Neotree focus last")
+				end
+			end,
+			desc = "[E]xplorer focus",
+			silent = true,
+		},
 		{ "<leader>ef", ":Neotree filesystem focus reveal<CR>", desc = "[E]xplorer [f]iles", silent = true },
 		{ "<leader>eb", ":Neotree buffers focus<CR>", desc = "[E]xplorer [b]uffers", silent = true },
-    { "<leader>eg", ":Neotree git_status focus<CR>", desc = "[E]xplorer [g]it status", silent = true },
+		{ "<leader>eg", ":Neotree git_status focus<CR>", desc = "[E]xplorer [g]it status", silent = true },
 	},
 }
