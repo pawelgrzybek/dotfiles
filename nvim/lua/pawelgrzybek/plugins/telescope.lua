6d2460b3af71fea95ab88099ace5958d0ea1cf66
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-12-09 20:54:52 +0000

    telescope cofig cleanup

--- nvim/lua/pawelgrzybek/plugins/telescope.lua
+++ nvim/lua/pawelgrzybek/plugins/telescope.lua
@@ -73,7 +73,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
 
-		-- See `:help telescope.builtin`
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [f]iles" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [b]uffers" })
@@ -84,30 +83,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind [c]urrent [w]ord" })
 		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [d]iagnostics" })
 		vim.keymap.set("n", "<leader><leader>", builtin.resume, { desc = "Find  (recent)" })
-
-		-- vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		-- vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		-- vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
-		-- Slightly advanced example of overriding default behavior and theme
-		-- vim.keymap.set("n", "<leader>/", function()
-		-- 	-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-		-- 	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-		-- 		winblend = 10,
-		-- 		previewer = false,
-		-- 	}))
-		-- end, { desc = "[/] Fuzzily search in current buffer" })
-
-		-- It's also possible to pass additional configuration options.
-		--  See `:help telescope.builtin.live_grep()` for information about particular keys
-		-- vim.keymap.set("n", "<leader>s/", function()
-		-- 	builtin.live_grep({
-		-- 		grep_open_files = true,
-		-- 		prompt_title = "Live Grep in Open Files",
-		-- 	})
-		-- end, { desc = "[S]earch [/] in Open Files" })
-
-		-- Shortcut for searching your Neovim configuration files
 		vim.keymap.set("n", "<leader>fc", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
 		end, { desc = "[F]ind [c]onfig files" })
