e2ce37d2ec747abf1e0695a3831e73cec5209c81
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-01-03 06:07:07 +0000

    up

--- nvim/lua/pawelgrzybek/plugins/oil.lua
+++ nvim/lua/pawelgrzybek/plugins/oil.lua
@@ -1,11 +1,3 @@
 return {
 	"stevearc/oil.nvim",
-	---@module 'oil'
-	---@type oil.SetupOpts
-	opts = {
-		default_file_explorer = false,
-	},
-	-- Optional dependencies
-	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
