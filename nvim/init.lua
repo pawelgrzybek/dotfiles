3be6507a4b4d7d0559fde4a11957d87cbefbfa76
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-01-13 08:43:29 +0000

    plugins update

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,6 @@ require("pawelgrzybek.options")
 require("pawelgrzybek.keymaps")
 require("pawelgrzybek.autocmds")
 require("pawelgrzybek.lazy")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
