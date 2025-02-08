e77c372bdd451c1cda3e3169bcbd3664a3123f58
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-02-08 12:47:02 +0000

    better oil config

--- nvim/lua/pawelgrzybek/plugins/oil.lua
+++ nvim/lua/pawelgrzybek/plugins/oil.lua
@@ -1,6 +1,25 @@
 return {
 	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
 	opts = {
-		default_file_explorer = true,
+		win_options = {
+			cursorcolumn = true,
+		},
+		columns = {},
+		float = {
+			padding = 3,
+			border = "single",
+		},
+		confirmation = {
+			border = "single",
+		},
+		progress = {
+			border = "single",
+		},
+		keymaps_help = {
+			border = "single",
+		},
 	},
+	lazy = false,
 }
