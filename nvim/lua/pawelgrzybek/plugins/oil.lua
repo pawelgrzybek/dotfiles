f751a7e278b8b36921e7049e7cef482eadad3f02
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-02-09 20:30:38 +0000

    up

--- nvim/lua/pawelgrzybek/plugins/oil.lua
+++ nvim/lua/pawelgrzybek/plugins/oil.lua
@@ -22,7 +22,7 @@ return {
 		})
 
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-		vim.keymap.set("n", "_", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Toggle floating Oil buffer" })
 	end,
 	lazy = false,
 }
