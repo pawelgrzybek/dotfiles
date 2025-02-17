bb4f226527c88a0c07417dea946556df2365abaf
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-02-17 19:52:35 +0000

    up

--- nvim/lua/pawelgrzybek/plugins/copilot-chat.lua
+++ nvim/lua/pawelgrzybek/plugins/copilot-chat.lua
@@ -6,9 +6,25 @@ return {
 			{ "nvim-lua/plenary.nvim", branch = "master" },
 		},
 		build = "make tiktoken",
-		opts = {
-			-- See Configuration section for options
-		},
+		config = function()
+			require("CopilotChat").setup()
+
+			-- this may not be needed in neovim 0.11
+			vim.api.nvim_create_autocmd("BufEnter", {
+				pattern = "copilot-*",
+				callback = function()
+					vim.opt.completeopt = vim.opt.completeopt + "noinsert" + "noselect" + "menuone" + "popup"
+				end,
+			})
+
+			-- this may not be needed in neovim 0.11
+			vim.api.nvim_create_autocmd("BufLeave", {
+				pattern = "copilot-*",
+				callback = function()
+					vim.opt.completeopt = vim.opt.completeopt - "noinsert" - "noselect" + "menuone" + "popup"
+				end,
+			})
+		end,
 		-- See Commands section for default commands if you want to lazy load on them
 	},
 }
