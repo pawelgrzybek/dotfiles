310db1b2b53fe096a1633d7206c7aa1ee06cf9de
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-03-01 08:48:12 +0000

    lint config

--- nvim/lua/pawelgrzybek/plugins/nvim-lint.lua
+++ nvim/lua/pawelgrzybek/plugins/nvim-lint.lua
@@ -1,6 +1,5 @@
 return {
-
-	{ -- Linting
+	{
 		"mfussenegger/nvim-lint",
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
@@ -21,17 +20,10 @@ return {
 				} or { "eslint_d" },
 			}
 
-			-- Create autocommand which carries out the actual linting
-			-- on the specified events.
-			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-				group = lint_augroup,
 				callback = function()
-					-- Only run the linter in buffers that you can modify in order to
-					-- avoid superfluous noise, notably within the handy LSP pop-ups that
-					-- describe the hovered symbol using Markdown.
 					if vim.opt_local.modifiable:get() then
-						lint.try_lint()
+						lint.try_lint(nil, { ignore_errors = true })
 					end
 				end,
 			})
