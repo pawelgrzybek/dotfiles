382090910cec26203391a3f04deb6df5529fffb5
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-03-06 05:45:35 +0000

    no bold

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,13 +1,13 @@
 function fish_mode_prompt
     switch $fish_bind_mode
         case default
-            set_color green --bold
+            set_color green
             echo '[N] '
         case insert
-            set_color brblack --bold
+            set_color brblack
             echo '[I] '
         case visual
-            set_color green --bold
+            set_color green
             echo '[V] '
     end
 end
