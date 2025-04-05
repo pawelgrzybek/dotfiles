6096be9315b8fec7c16527f7684402cba3665efa
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-04-05 08:39:08 +0100

    fix lsp member highlighting

--- nvim/lua/pawelgrzybek/autocmds.lua
+++ nvim/lua/pawelgrzybek/autocmds.lua
@@ -113,7 +113,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- code, if the language server you are using supports them
 		-- This may be unwanted, since they displace some of your code
 		local client = vim.lsp.get_client_by_id(event.data.client_id)
-		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
 			vim.lsp.inlay_hint.enable(true)
 
 			vim.keymap.set("n", "<leader>sh", function()
@@ -126,18 +126,26 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		--    See `:help CursorHold` for information about when this is executed
 		--
 		-- When you move your cursor, the highlights will be cleared (the second autocommand).
-		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
 			local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 				buffer = event.buf,
 				group = highlight_augroup,
 				callback = vim.lsp.buf.document_highlight,
 			})
-			--
+
 			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 				buffer = event.buf,
 				group = highlight_augroup,
 				callback = vim.lsp.buf.clear_references,
+			})
+
+			vim.api.nvim_create_autocmd("LspDetach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+				callback = function(event2)
+					vim.lsp.buf.clear_references()
+					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+				end,
 			})
 
 			vim.api.nvim_create_autocmd("LspDetach", {
