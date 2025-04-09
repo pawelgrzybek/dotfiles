f5eabdf188003b366a6a1cff01ac36b546e70240
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-04-09 11:51:31 +0100

    disable buffer completions

--- nvim/lua/pawelgrzybek/plugins/blink.lua
+++ nvim/lua/pawelgrzybek/plugins/blink.lua
@@ -53,7 +53,8 @@ return {
 		-- default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "path", "snippets" },
+			-- default = { "lsp", "path", "snippets", "buffer" },
 			-- optionally disable cmdline completions
 			-- cmdline = {},
 		},
