7fe0c222f2a8d5abf191516a282d5f46a2224f52
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-04-23 06:53:56 +0100

    extend the scop of mini.ai

--- nvim/lua/pawelgrzybek/plugins/mini.lua
+++ nvim/lua/pawelgrzybek/plugins/mini.lua
@@ -2,7 +2,9 @@ return {
 	"echasnovski/mini.nvim",
 	version = false,
 	config = function()
-		require("mini.ai").setup()
+		require("mini.ai").setup({
+			n_lines = 500,
+		})
 		require("mini.surround").setup({})
 		require("mini.indentscope").setup({
 			draw = {
