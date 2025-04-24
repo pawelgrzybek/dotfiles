1c20d8c60f47b458f692445c87889287478df3ab
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-04-24 19:44:40 +0100

    up

--- nvim/lua/pawelgrzybek/options.lua
+++ nvim/lua/pawelgrzybek/options.lua
@@ -43,10 +43,8 @@ vim.g.markdown_fenced_languages = {
 }
 
 vim.diagnostic.config({
-	virtual_text = true,
-	virtual_lines = {
-		current_line = true,
-	},
+	-- virtual_text = true,
+	virtual_lines = true,
 	signs = {
 		text = {
 			[vim.diagnostic.severity.ERROR] = "󰅚 ",
