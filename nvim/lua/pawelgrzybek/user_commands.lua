d6e59c0841ab23a349a2820ade4ff708304c70b9
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-04-26 07:35:49 +0100

    add user commands to copy path to the clipboard

--- nvim/lua/pawelgrzybek/user_commands.lua
+++ nvim/lua/pawelgrzybek/user_commands.lua
@@ -2,3 +2,11 @@ vim.api.nvim_create_user_command("ESLintFix", function()
 	vim.cmd("silent !eslint_d --fix " .. vim.fn.expand("%"))
 	vim.cmd("redraw!")
 end, {})
+
+vim.api.nvim_create_user_command("PathToClipboardRelative", function()
+	vim.fn.setreg("+", vim.fn.expand("%"))
+end, {})
+
+vim.api.nvim_create_user_command("PathToClipboardAbsolute", function()
+	vim.fn.setreg("+", vim.fn.expand("%:p"))
+end, {})
