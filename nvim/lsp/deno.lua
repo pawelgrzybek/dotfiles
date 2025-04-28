b6b49efae9e3ad76d52110ef8ab500a00d78d6d5
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-04-28 06:08:06 +0100

    even better

--- nvim/lsp/deno.lua
+++ nvim/lsp/deno.lua
@@ -1,17 +1,19 @@
 return {
 	cmd = { "deno", "lsp" },
 	cmd_env = { NO_COLOR = true },
+	root_markers = { "deno.json" },
+	workspace_required = true,
 	---@diagnostic disable-next-line: unused-local
-	root_dir = function(bufnr, on_dir)
-		local root_path = vim.fs.find("deno.json", {
-			upward = true,
-			type = "file",
-			path = vim.fn.getcwd(),
-		})[1]
-		if root_path then
-			on_dir(vim.fn.fnamemodify(root_path, ":h"))
-		end
-	end,
+	-- root_dir = function(bufnr, on_dir)
+	-- 	local root_path = vim.fs.find("deno.json", {
+	-- 		upward = true,
+	-- 		type = "file",
+	-- 		path = vim.fn.getcwd(),
+	-- 	})[1]
+	-- 	if root_path then
+	-- 		on_dir(vim.fn.fnamemodify(root_path, ":h"))
+	-- 	end
+	-- end,
 	filetypes = {
 		"javascript",
 		"javascriptreact",
--- nvim/lsp/ts_ls.lua
+++ nvim/lsp/ts_ls.lua
@@ -1,16 +1,18 @@
 return {
 	cmd = { "typescript-language-server", "--stdio" },
+	root_markers = { "package.json" },
+	workspace_required = true,
 	---@diagnostic disable-next-line: unused-local
-	root_dir = function(bufnr, on_dir)
-		local root_path = vim.fs.find("package.json", {
-			upward = true,
-			type = "file",
-			path = vim.fn.getcwd(),
-		})[1]
-		if root_path then
-			on_dir(vim.fn.fnamemodify(root_path, ":h"))
-		end
-	end,
+	-- root_dir = function(bufnr, on_dir)
+	-- 	local root_path = vim.fs.find("package.json", {
+	-- 		upward = true,
+	-- 		type = "file",
+	-- 		path = vim.fn.getcwd(),
+	-- 	})[1]
+	-- 	if root_path then
+	-- 		on_dir(vim.fn.fnamemodify(root_path, ":h"))
+	-- 	end
+	-- end,
 	filetypes = {
 		"javascript",
 		"javascriptreact",
