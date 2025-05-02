85a9fb9d5f6533f14c60cb27de6e6c72cdae5bc5
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-05-02 16:57:22 +0100

    move between windows in nvim easily

--- nvim/lua/pawelgrzybek/keymaps.lua
+++ nvim/lua/pawelgrzybek/keymaps.lua
@@ -85,3 +85,8 @@ vim.keymap.set("n", "<leader>v", function()
 		vim.cmd("DiffviewClose")
 	end
 end)
+
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
