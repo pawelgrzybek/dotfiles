0ad82300a21991b9b6bfca863d5085065a7413e1
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-05-08 07:01:22 +0100

    enable blink integreatin with lazydev

--- nvim/lua/pawelgrzybek/plugins/blink.lua
+++ nvim/lua/pawelgrzybek/plugins/blink.lua
@@ -53,7 +53,15 @@ return {
 		-- default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "path", "snippets" },
+			default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					-- make lazydev completions top priority (see `:h blink.cmp`)
+					score_offset = 100,
+				},
+			},
 			-- default = { "lsp", "path", "snippets", "buffer" },
 			-- optionally disable cmdline completions
 			-- cmdline = {},
