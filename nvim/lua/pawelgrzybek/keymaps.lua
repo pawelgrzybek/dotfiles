abb29f2d57469ef58baf769a8168bae723738aae
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-05-19 07:37:57 +0100

    simplify some keymaps

--- nvim/lua/pawelgrzybek/keymaps.lua
+++ nvim/lua/pawelgrzybek/keymaps.lua
@@ -1,4 +1,4 @@
--- TIP: Disable arrow keys in normal mode
+-- Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
@@ -9,14 +9,10 @@ vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
 vim.keymap.set("n", "<A-J>", ":t.<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<A-K>", ":t-1<CR>", { noremap = true, silent = true })
-
--- Move/duplicate lines up and down (insert mode)
 vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
 vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
 vim.keymap.set("i", "<A-J>", "<C-o>:t.<CR>", { noremap = true, silent = true })
 vim.keymap.set("i", "<A-K>", "<C-o>:t-1<CR>", { noremap = true, silent = true })
-
--- Move/duplicate lines up and down (visual mode)
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<A-J>", ":t-1<CR>gv", { noremap = true, silent = true })
@@ -34,7 +30,7 @@ vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { desc = "Open [d]ia
 vim.keymap.set("n", "<leader><A-d>", vim.diagnostic.open_float, { desc = "Open [d]iagnostics (float)" })
 vim.keymap.set("n", "<leader>D", vim.diagnostic.setqflist, { desc = "Open [D]iagnostics (quickfix list)" })
 
--- Quickfix stuff
+-- Toggle quickfix/location list
 vim.keymap.set("n", "<leader>q", function()
 	local windows = vim.fn.getwininfo()
 	for _, win in pairs(windows) do
@@ -45,11 +41,6 @@ vim.keymap.set("n", "<leader>q", function()
 	end
 	vim.cmd("copen")
 end, { desc = "Toggle quickfix list" })
--- looks like in 0.11 it is the default
--- vim.keymap.set("n", "[q", "<cmd>:cprev<Cr>", { desc = "Quick fix next" })
--- vim.keymap.set("n", "]q", "<cmd>:cnext<Cr>", { desc = "Quick fix next" })
-
--- Location list stuff
 vim.keymap.set("n", "<leader>l", function()
 	local windows = vim.fn.getwininfo()
 	for _, win in pairs(windows) do
@@ -60,24 +51,8 @@ vim.keymap.set("n", "<leader>l", function()
 	end
 	vim.cmd("lopen")
 end, { desc = "Toggle locatoin list" })
--- looks like in 0.11 it is the default
--- vim.keymap.set("n", "[l", "<cmd>:lprev<Cr>", { desc = "Location list previous" })
--- vim.keymap.set("n", "]l", "<cmd>:lnext<Cr>", { desc = "Location list next" })
 
--- looks like in 0.11 it is the default
--- quickly navigate between buffers
--- vim.keymap.set("n", "[b", "<cmd>:bprevious<Cr>")
--- vim.keymap.set("n", "]b", "<cmd>:bnext<Cr>")
--- vim.keymap.set("n", "[B", "<cmd>:bfirst<Cr>")
--- vim.keymap.set("n", "]B", "<cmd>:blast<Cr>")
-
--- looks like in 0.11 it is the default
--- quickly navigate between tabs
--- vim.keymap.set("n", "[t", "<cmd>:tabprevious<Cr>")
--- vim.keymap.set("n", "]t", "<cmd>:tabnext<Cr>")
--- vim.keymap.set("n", "[T", "<cmd>:tabfirst<Cr>")
--- vim.keymap.set("n", "]T", "<cmd>:tablast<Cr>")
---
+-- Toggle diffview
 vim.keymap.set("n", "<leader>v", function()
 	if next(require("diffview.lib").views) == nil then
 		vim.cmd("DiffviewOpen")
@@ -85,8 +60,3 @@ vim.keymap.set("n", "<leader>v", function()
 		vim.cmd("DiffviewClose")
 	end
 end)
-
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
