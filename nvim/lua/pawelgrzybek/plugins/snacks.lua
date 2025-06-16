82249a9a6d37eac295b393d914ee9739864688c7
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-06-16 08:45:10 +0100

    animation is annoying in this case, one day to refine

--- nvim/lua/pawelgrzybek/plugins/snacks.lua
+++ nvim/lua/pawelgrzybek/plugins/snacks.lua
@@ -2,12 +2,6 @@ return {
 	"folke/snacks.nvim",
 	---@type snacks.Config
 	opts = {
-		scroll = {
-			animate = {
-				duration = { step = 8, total = 80 },
-				easing = "linear",
-			},
-		},
 		picker = {
 			layout = {
 				layout = {
