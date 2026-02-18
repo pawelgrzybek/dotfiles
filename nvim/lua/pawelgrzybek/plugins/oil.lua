2ef4d81ab72a71ab625a1b3d2eca84b25064e1bf
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-02-18 07:09:11 +0000

    fix coliding keymaps in oil plugin

--- nvim/lua/pawelgrzybek/plugins/oil.lua
+++ nvim/lua/pawelgrzybek/plugins/oil.lua
@@ -19,6 +19,37 @@ return {
 			keymaps_help = {
 				border = "single",
 			},
+			watch_for_changes = true,
+			keymaps = {
+				-- i use these two for window navigation
+				["<C-l>"] = false,
+				["<C-h>"] = false,
+				-- the default is <c-l>, which i use for window navigation
+				-- also, r for refresh makes more sense to me
+				["<C-r>"] = {
+					callback = "actions.refresh",
+					desc = "Refresh oil",
+					mode = "n",
+				},
+				-- the default is <c-l>, which i use for window navigation
+				-- also, r for refresh makes more sense to me
+				["<C-s>"] = {
+					callback = "actions.select",
+					desc = "Split (horizontal)",
+					mode = "n",
+					opts = {
+						horizontal = true,
+					},
+				},
+				["<C-v>"] = {
+					callback = "actions.select",
+					desc = "Split (vertival)",
+					mode = "n",
+					opts = {
+						vertical = true,
+					},
+				},
+			},
 		})
 
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
