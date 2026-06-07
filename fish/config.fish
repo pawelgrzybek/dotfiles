ce5bd7b4f041d890c7b1b89d4dd9b36d1887319b
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-06-07 20:33:44 +0100

    improce yolo brew script

--- fish/config.fish
+++ fish/config.fish
@@ -47,7 +47,7 @@ abbr --add cp cp -riv
 abbr --add mkdir mkdir -vp
 abbr --add ls ls -F
 abbr --add ll ls -la
-abbr --add yolo "brew upgrade"
+abbr --add yolo "brew upgrade && brew autoremove && brew cleanup"
 abbr --add byebye_node_modules find . -name node_modules -type d -prune -exec rm -rf '{}' +
 
 # some git shortcuts
