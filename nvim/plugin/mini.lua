c8a5b8e26627748835a56580a50e6bab0c2cd658
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-07-22 07:11:47 +0100

    mini clue

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -28,6 +28,46 @@ require("mini.files").setup({
 	},
 })
 
+local miniclue = require("mini.clue")
+miniclue.setup({
+	triggers = {
+		-- Leader triggers
+		{ mode = { "n", "x" }, keys = "<Leader>" },
+
+		-- `[` and `]` keys
+		{ mode = "n", keys = "[" },
+		{ mode = "n", keys = "]" },
+
+		-- `g` key
+		{ mode = { "n", "x" }, keys = "g" },
+
+		-- Marks
+		{ mode = { "n", "x" }, keys = "'" },
+		{ mode = { "n", "x" }, keys = "`" },
+
+		-- Registers
+		{ mode = { "n", "x" }, keys = '"' },
+		{ mode = { "i", "c" }, keys = "<C-r>" },
+
+		-- Window commands
+		{ mode = "n", keys = "<C-w>" },
+
+		-- `z` key
+		{ mode = { "n", "x" }, keys = "z" },
+	},
+
+	clues = {
+		-- Enhance this by adding descriptions for <Leader> mapping groups
+		miniclue.gen_clues.square_brackets(),
+		miniclue.gen_clues.builtin_completion(),
+		miniclue.gen_clues.g(),
+		miniclue.gen_clues.marks(),
+		miniclue.gen_clues.registers(),
+		miniclue.gen_clues.windows(),
+		miniclue.gen_clues.z(),
+	},
+})
+
 vim.keymap.set("n", "<leader>-", function()
 	local mf = require("mini.files")
 	if not mf.close() then
