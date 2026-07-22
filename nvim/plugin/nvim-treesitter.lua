b31839dffd498543519ac1ca76e62a3d78df6d20
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-07-22 06:45:40 +0100

    berrer type syfe call for language.add on tresitter

--- nvim/plugin/nvim-treesitter.lua
+++ nvim/plugin/nvim-treesitter.lua
@@ -34,7 +34,7 @@ vim.api.nvim_create_autocmd("FileType", { -- enable treesitter highlighting and 
 	callback = function(args)
 		local filetype = args.match
 		local lang = vim.treesitter.language.get_lang(filetype)
-		if vim.treesitter.language.add(lang) then
+		if lang and vim.treesitter.language.add(lang) then
 			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 			vim.treesitter.start()
 		end
