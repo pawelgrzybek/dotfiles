fce1b24417464aaddf4e47c80ff88bd75e41f9f7
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-08-13 21:39:49 +0100

    simpler nvim-lint

--- nvim/plugin/nvim-lint.lua
+++ nvim/plugin/nvim-lint.lua
@@ -12,7 +12,7 @@ lint.linters_by_ft = {
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 	callback = function()
-		if vim.opt_local.modifiable:get() then
+		if vim.bo.modifiable then
 			lint.try_lint(nil, { ignore_errors = true })
 		end
 	end,
