6d928c600283df0216616fab69aa2b3f818f406e
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2026-09-03 07:48:57 +0100

    config updat

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+# The following lines were added by Docker Desktop to add commands to your PATH.
+export PATH="$PATH:/Users/pawelgrzybek/.docker/bin"
+# End of Docker Desktop section.
+
 fish_add_path -gPm /opt/homebrew/bin /Applications/Ghostty.app/Contents/MacOS
 
 set -gx EDITOR nvim
